80c19ec9ba9d03408f1854d731f0b39a082d0a9b
Allister <allisterkmoon@gmail.com>
2017-07-11 22:08:24 -0700

    Pretty up about section and background

--- js/main.js
+++ js/main.js
@@ -1,14 +1,15 @@
-var vuePortfolio = new Vue({
-  el: '#portfolio',
-  data: {
-    clientProjects: clientProjects,
-    fccProjects: fccProjects.reverse()
-  }
-})
+var skills = {
+  'front-end': ['HTML', 'CSS', 'JavaScript', 'JQuery', 'Bootstrap', 'React', 'Redux', 'Vue'],
+  'back-end': ['Node.js', 'Express', 'Electron.js'],
+  'other': ['C++', 'Python', 'Linux Server', 'Network Administration']
+}
 
-var vueYear = new Vue({
-  el: '.trademark',
+var app = new Vue({
+  el: '#app',
   data: {
+    skills: skills,
+    clientProjects: clientProjects,
+    fccProjects: fccProjects.reverse(),
     year: (new Date()).getFullYear()
   }
 })
