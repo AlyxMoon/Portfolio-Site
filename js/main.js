7bc8853c556f8036a7f649f5e289bf666a699f69
Allister <allisterkmoon@gmail.com>
2019-07-29 16:13:25 -0700

    Mostly clear out site to start a fresh one

--- js/main.js
+++ js/main.js
@@ -1,15 +1,6 @@
-var skills = {
-  'front-end': ['HTML', 'CSS', 'JavaScript', 'JQuery', 'Bootstrap', 'React', 'Redux', 'Vue'],
-  'back-end': ['Node.js', 'Express', 'Electron.js'],
-  'other': ['C++', 'Python', 'Linux Server', 'Network Administration']
-}
-
 var app = new Vue({
   el: '#app',
   data: {
-    skills: skills,
-    clientProjects: clientProjects,
-    fccProjects: fccProjects.reverse(),
     year: (new Date()).getFullYear()
   }
 })
