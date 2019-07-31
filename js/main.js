a53d54c30ffd0066bd012e80bcd1c477eaaa4771
Allister <allisterkmoon@gmail.com>
2019-07-30 19:05:45 -0700

    Add in a content section with brief about me

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
+var skills = {
+  'web': ['HTML', 'CSS', 'CSS Animations', 'JavaScript'],
+  'frontend': ['JQuery', 'Bootstrap', 'Vue', 'React', 'Angular'],
+  'backend': ['Node', 'Express', 'Feathers'],
+  'other': ['Electron']
+}
+
 var app = new Vue({
   el: '#app',
   data: {
-    year: (new Date()).getFullYear()
+    year: (new Date()).getFullYear(),
+    skills,
+    showSkills: false
   }
 })
